35a21a35093a5a44fa123d16b987716ebad83f3e
Fabrice Hafashimana <feytoninc@hotmail.com>
2022-03-19 22:48:35 +0200

    Configure and moves to the AWS for static Files
    - Add AWS Bucket configuration
    - Move existing statics to AWS
    - Configure media uploads

--- corp/settings/development.py
+++ corp/settings/development.py
@@ -4,10 +4,8 @@ from decouple import config
 
 from .base import *
 
-DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'www.localhost']
 
-SECRET_KEY = config('SECRET_KEY')
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL SETTINGS
@@ -18,25 +16,11 @@ EMAIL_HOST_USER = 'your email'
 EMAIL_HOST_PASSWORD = 'password'
 EMAIL_USE_SSL = False
 
-STRIPE_PUBLISHABLE_KEY = config("STRIPE_TEST_PUBLIC")
-STRIPE_SECRET_KEY = config("STRIPE_TEST_SECRET")
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-STATIC_ROOT = (os.path.join(BASE_DIR, 'asset'))
-STATIC_URL = '/static/'
-
-
-# media root folder
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 SITE_ID = 1
